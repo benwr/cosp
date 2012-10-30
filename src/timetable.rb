28f74579472dd3d5db10ab621044d7445638a09a
Ben Weinstein-Raun <bwr@vt.edu>
2012-10-30 00:17:50 -0400

    added rudimentary query capability

--- src/timetable.rb
+++ src/timetable.rb
@@ -4,36 +4,21 @@ require 'open-uri'
 require 'nokogiri'
 
 
-def get_course (subject, id)
-  subject = subject.upcase
-  term = 201201
-  number = id
-
-  found = false;
-
-  crn = 0;
-  
-  
-  while (not found)
-    url = base_url + ".P_ProcRequest?\
-history=Y;CAMPUS=0&TERMYEAR=#{term}&CORE_CODE=AR%25&SUBJ_CODE=#{subject}\
-&SCHDTYPE=%25&CRSE_NUMBER=#{number}&crn=&open_only=\
-&BTN_PRESSED=FIND+class+sections&inst_name=&PRINT_FRIEND=Y"
-    response = Nokogiri::HTML(open(url))
-    response.xpath("//font[@size=1]").each do |field|
-      if field.text =~ /^\d+$/
-        found = true
-        crn = field.text.to_i
-      end
-    end
+#    response = Nokogiri::HTML(open(url))
+#    response.xpath("//font[@size=1]").each do |field|
+#      if field.text =~ /^\d+$/
+#        found = true
+#        crn = field.text.to_i
+#      end
+#    end
     
-  end
-end
+#  end
+#end
 
 class Timetable
   def initialize(url="https://banweb.banner.vt.edu/ssb/prod/HZSKVTSC", 
                  sem='201301',
-                 search_format=".P_ProcRequest?history=Y&CAMPUS=0&TERMYEAR=%{term}&CORE_CODE=AR%%25&SUBJ_CODE=%{subject}&SCHDTYPE=%%25&CRSE_NUMBER=%{number}&crn=&open_only=&BTN_PRESSED=FIND+class+sections&inst_name=&PRINT_FRIEND=Y")
+                 search_format=".P_ProcRequest?history=Y&CAMPUS=0&TERMYEAR=%{term}&CORE_CODE=AR%%25&SUBJ_CODE=%{subject}&SCHDTYPE=%%25&CRSE_NUMBER=%{number}&crn=&open_only=&BTN_PRESSED=FIND+class+sections&inst_name=") #&PRINT_FRIEND=Y")
     @url = url
     @current_term = sem
     @search_format = search_format
@@ -42,19 +27,34 @@ class Timetable
   def get_course(subj, id)  # returns a hash table with subject, id, title, 
     term = @current_term    # description, example crn, credit count, prereqs, 
     results = []            # coreqs
-    while (!(results = search(term, subj, id)))
-      term = self.previous_semester(term)
+    while (not (results = search(term, subj, id)))
+      term = previous_semester(term)
     end
+    return results
+  end
+
+  def search(term, subj, id) # returns a 2d array of the results
+    search_string = @search_format % {   :term => term,
+                                      :subject => subj,
+                                       :number => id}
+    resp = Nokogiri::HTML(open(@url + search_string))
+    result = []
+    rows = resp.xpath('//tr')[14..-1]
+    return nil if rows.nil?
+    rows.each do |row|
+      tds = row.css('td')
+      break unless tds.length == 12
+      result.push({:subject => subj,
+                    :number => id,
+                   :credits => tds[4].text.strip.to_i,
+                     :title => tds[2].text.strip,
+                       :crn => tds[0].text.gsub(/\s|(&nbsp)/, '')})
+    end
+
+    return result
   end
 
   private
-  def search(term, subj, id) # returns a 2d array of the results
-    search_string = search_format % {   :term => term,
-                                     :subject => subj,
-                                      :number => id}
-    resp = Nokogiri::HTML(open(@url + search_string))
-    puts resp.to_s
-  end
 
   def previous_semester(sem)
     # semester strings are yyyytt, where tt = 01 means spring, tt = 07 
