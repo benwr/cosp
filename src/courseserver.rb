47800d4663f980cf819c1c50369b4869e7b5b857
Ben Weinstein-Raun <benwr@vt.edu>
2012-10-31 14:32:39 -0700

    add search-by-semester capability

--- src/courseserver.rb
+++ src/courseserver.rb
@@ -2,13 +2,23 @@ require 'sinatra'
 require './timetable.rb'
 require 'json'
 
+CURRENT_SEMESTER = '201301'
+
 get '/' do
   "To find info on, say, ENGE 1024, GET /ENGE/1024"
 end
 
-get '/search/:subj/:num' do
+get '/detail/:subj/:num' do
   t = Timetable.new
-  JSON::dump(t.search('201301', params[:subj], params[:num], false))
+  JSON::dump(t.search(CURRENT_SEMESTER, params[:subj], params[:num], false))
+end
+
+get '/detail/:sem/:subj/:num' do
+  t = Timetable.new
+  JSON::dump(t.search(params[:sem], 
+                      params[:subj], 
+                      params[:num], 
+                      params[:sem].to_i == CURRENT_SEMESTER.to_i ))
 end
 
 get '/:subj/:num' do
